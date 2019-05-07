bdd1959d724ee85a7f680496a23670a67f699cd9
Jemima Adjanor <jemimaadjanor@yahoo.com>
2019-05-07 12:56:44 +0100

    adding changes to movieSearch app

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -15,18 +15,9 @@ class Movies extends Component{
 }
 
 componentDidMount(){
-<<<<<<< HEAD
-=======
-    this.moviesOMovies()
->>>>>>> 5ea4c2e85dc09e929cd9705c5406dac563a7cc86
     let searchButton = document.getElementById('search')
     searchButton.addEventListener('click', ()=>{this.moviesOMovies()})
 }
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 5ea4c2e85dc09e929cd9705c5406dac563a7cc86
 
 async moviesOMovies(){
     try {
@@ -63,15 +54,9 @@ render(){
                 <button className='searchButton' type='button' id='search'>Search</button>
             </div>
             <div className='movieDisplayInfo'>
-<<<<<<< HEAD
 
                {myMovieSearch}
                
-=======
-                <a target='_blank' href={this.state.search.Website} alt='www.youtube.com'><img src={this.state.search.Poster}/></a>
-                <p>{this.state.search.Plot}</p>
-                <a target='_blank' href={this.state.search.Website}>{this.state.search.Website}</a>
->>>>>>> 5ea4c2e85dc09e929cd9705c5406dac563a7cc86
             </div>
             
 </div>
